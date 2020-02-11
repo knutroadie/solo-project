58db9e9d5ba2df282977fa8c17ee9983fe863c49
luke rohde <morneau33@gmail.com>
2020-02-11 17:55:32 -0600

    removed unneeded lines in logoutbutton and appjs

--- src/components/LogOutButton/LogOutButton.js
+++ src/components/LogOutButton/LogOutButton.js
@@ -12,13 +12,9 @@ const LogOutButton = props => (
   >logout</button>
 );
 
-const putReduxStateOnProps = (reduxState) => {
-  return {
-      reduxState
-  }
-}
-// This component doesn't need 'mapStateToProps'
+// // This component doesn't need 'mapStateToProps'
 // because it doesn't care what the current state is.
 // No matter what the redux state is, this button will always be a log out button
 // this component still needs 'connect' though, because it is going to dispatch a redux action
+
 export default withRouter(connect()(LogOutButton));
