e1855050982006b1a39f8b75cf307f02685df39e
luke rohde <morneau33@gmail.com>
2020-02-11 15:28:29 -0600

    confirm alert on admin delete button

--- src/components/OppDetail/OppDetail.js
+++ src/components/OppDetail/OppDetail.js
@@ -9,7 +9,6 @@ class OppDetail extends Component {
         this.props.history.push(`/edit`)
     }
     deleteOpp = () => {
-        alert('are you sure you want to delete that?')
         console.log('clicking to delete');
         this.props.history.push(`/`)
     }
@@ -20,7 +19,7 @@ class OppDetail extends Component {
             <div>
                 <p>this is an ITEM</p>
                 <button onClick={this.goEdit}>edit</button>
-                <button onClick={this.deleteOpp}>delete</button>
+                <button onClick={(event) => {if (window.confirm('are you sure you want to delete this?')) this.deleteOpp(event)}}>delete</button>
             </div>
         )
     }
