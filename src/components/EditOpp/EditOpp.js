25825e167228eab67f862e980ff6e00094595283
luke rohde <morneau33@gmail.com>
2020-02-14 14:27:08 -0600

    pre-populating edit input fields

--- src/components/EditOpp/EditOpp.js
+++ src/components/EditOpp/EditOpp.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
@@ -14,46 +14,47 @@ class EditOpp extends Component {
     // let oppId = this.props.match.params.id;
 
     state = {
-        editedOpp: {
+        oppToEdit: {
             img_url: '',
-            name: '',
-            description: '',
-            contact: '',
-            email: '',
-            phone: '',
-            web_address: '',
-            social: '',
-            street_address: '',
-            city: '',
-            zip: '',
+            name: this.props.reduxState.detailReducer.name,
+            description: this.props.reduxState.detailReducer.description,
+            contact: this.props.reduxState.detailReducer.contact,
+            email: this.props.reduxState.detailReducer.email,
+            phone: this.props.reduxState.detailReducer.phone,
+            web_address: this.props.reduxState.detailReducer.web_address,
+            social: this.props.reduxState.detailReducer.social,
+            street_address: this.props.reduxState.detailReducer.street_address,
+            city: this.props.reduxState.detailReducer.city,
+            zip: this.props.reduxState.detailReducer.zip
         }
     }
 
-submitEdit = () => {
-    console.log('clicking to submit edit', this.state.editedOpp);
-    this.goDetail();
-}
+    submitEdit = () => {
+        console.log('clicking to submit edit', this.state.oppToEdit);
+        this.goDetail();
+    }
 
     goDetail = () => {
         console.log('clicking to go detail', this.props.params.id);
-        this.props.history.push(`/detail`)
+        this.props.history.push(`/detail/${this.props.params.id}`)
     }
 
     render() {
+        console.log(this.props.reduxState.detailReducer);
         return (
             <div>
                 <p>this is the EDIT form</p>
                 <input placeholder="enter an image_url"></input>
-                <input placeholder="name"></input>
-                <input placeholder="description"></input>
-                <input placeholder="contact"></input>
-                <input placeholder="email"></input>
-                <input placeholder="phone"></input>
-                <input placeholder="web address"></input>
-                <input placeholder="social"></input>
-                <input placeholder="street address"></input>
-                <input placeholder="city"></input>
-                <input placeholder="zip code"></input>
+                <input defaultValue={this.state.oppToEdit.name}></input>
+                <input defaultValue={this.state.oppToEdit.description}></input>
+                <input defaultValue={this.state.oppToEdit.contact}></input>
+                <input defaultValue={this.state.oppToEdit.email}></input>
+                <input defaultValue={this.state.oppToEdit.phone}></input>
+                <input defaultValue={this.state.oppToEdit.web_address}></input>
+                <input defaultValue={this.state.oppToEdit.social}></input>
+                <input defaultValue={this.state.oppToEdit.street_address}></input>
+                <input defaultValue={this.state.oppToEdit.city}></input>
+                <input defaultValue={this.state.oppToEdit.zip}></input>
                 <button onClick={this.submitEdit}>submit changes</button>
             </div>
         )
