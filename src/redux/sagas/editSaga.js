2b2cc5b802be9248da4567b8d657d4b42b935e4b
luke rohde <morneau33@gmail.com>
2020-02-14 18:00:21 -0600

    details page and home page re-rendering properly

--- src/redux/sagas/editSaga.js
+++ src/redux/sagas/editSaga.js
@@ -8,7 +8,6 @@ function* editSaga() {
 function* editOpp(action) {
     console.log('in editOpp', action);
     let response = yield axios.put(`/api/opportunity/${action.payload.id}`, action.payload);
-    yield put({ type: 'SET_OPP', payload: response.data[0] });
     yield put({ type: 'GET_OPPS' });
 }
 
